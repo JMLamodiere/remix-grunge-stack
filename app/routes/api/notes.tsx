7305c9b86b0d87dba73b2d53e20a317d79b926b9
Jean-Marie Lamodière <JMLamodiere@users.noreply.github.com>
2022-11-23 14:02:17 +0100

    feat: return 401 instead of redirect on HTTP API

--- app/routes/api/notes.tsx
+++ app/routes/api/notes.tsx
@@ -1,5 +1,10 @@
-import type { LoaderFunction } from "@remix-run/node";
+import type { LoaderArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { requireUserIdOr401 } from "~/session.server";
+import { getNoteListItems } from "~/models/note.server";
 
-import { loader as originalLoader } from "~/routes/notes";
-
-export const loader: LoaderFunction = originalLoader;
+export const loader = async ({ request }: LoaderArgs) => {
+  const userId = await requireUserIdOr401(request);
+  const noteListItems = await getNoteListItems({ userId });
+  return json({ noteListItems });
+};
